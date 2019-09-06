ce9116fd1853db77156660ba64d70b26edff2cba
sourav kumar <sourav.baranwal@gmail.com>
2019-09-06 23:46:39 +0530

    refactored hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,9 +4,7 @@ class Hero extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //   movieId: null,
-      videoKey: null,
-      isLoading: true
+      movie: null
     };
   }
 
@@ -16,35 +14,23 @@ class Hero extends Component {
     )
       .then(res => res.json())
       .then(({ results }) =>
-        fetch(
-          `https://api.themoviedb.org/3/movie/${results[0].id}/videos?api_key=aebe3910ab68767bd5047cf76f34c313&language=en-US
-      `
-        )
-          .then(res => res.json())
-          .then(data =>
-            this.setState({
-              videoKey: data.results[0].key,
-              isLoading: true
-            })
-          )
+        this.setState({
+          movie: results[1]
+        })
       );
   }
   render() {
-    if (this.state.videoKey) {
+    if (this.state.movie) {
       return (
         <>
           <div className="hero">
-            {/* Hi I am Header and I am working properly
-            {this.state.videoKey} */}
-            <div className="iframe-container">
-              <iframe
-                id="bg"
-                src={`https://www.youtube.com/embed/${this.state.videoKey}?controls=0&amp;autoplay=0&amp`}
-                frameborder="0"
-                autoplay="1"
-              ></iframe>
+            <div className="img-container">
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${this.state.movie.backdrop_path}`}
+                alt=""
+              />
             </div>
-            <div className="iframe-content">
+            <div className="img-content">
               <h2>This will be placed over the video</h2>
             </div>
           </div>
